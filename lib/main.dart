bf787bc40f5678cd66e5be5a3149a1356f67c67c
mestreian <ghenrique737@gmail.com>
2020-01-02 15:42:11 -0300

    Studying Button Animation

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:animation_button/ButtonAnimation.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  )
+);
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ButtonAnimation(Color.fromRGBO(57, 92, 249, 1), Color.fromRGBO(44, 78, 233, 1)),
+              SizedBox(height: 20,),
+              
+              ButtonAnimation(Colors.yellow[700], Colors.yellow[800]),
+              SizedBox(height: 20,),
+              
+              ButtonAnimation(Colors.green[700], Colors.green[800]),
+              SizedBox(height: 20,),
+             
+              ButtonAnimation(Colors.red[700], Colors.red[800]),
+              SizedBox(height: 20,),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
